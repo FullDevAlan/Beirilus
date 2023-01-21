8b635566aeb055adae47c114eb53a177245ecb04
FullDevAlan <111159204+FullDevAlan@users.noreply.github.com>
2023-01-21 20:21:25 -0300

    revertendo commits

--- src/component/Pages/Login/index.tsx
+++ src/component/Pages/Login/index.tsx
@@ -1,46 +1,67 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { loginClientes } from '../../services/LogApi/clientes';
 import "./styles.css";
 
+export default function Login(): JSX.Element {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
-const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const login = async (event: FormEvent) => {
+        event.preventDefault();
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
+        const payload = {
+            email,
+            password,
+        };
 
-        console.log("submit", { email, password });
+        try {
+            const response = await loginClientes(payload);
+
+            if (response.status !== 201) {
+                return alert("Deu algo errado");
+            }
+
+            alert("Login efetuado com sucesso!");
+        } catch (error) {
+            alert("Deu algo errado");
+        }
     };
 
     return (
-        <div id="login">
-            <h1 className="title">Login do sitema</h1>
-            <form className="form" onSubmit={handleSubmit}>
-                <div className="field">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="field">
-                    <label htmlFor="passoword" >Senha</label>
-                    <input
-                        type="passoword"
-                        name="passoword"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)} />
-
-                </div>
-                <div className="actions">
-                    <button type="submit">Entrar</button>
+        <div>
+            <form className="login-form" onSubmit={login}>
+                <div className="wrap-input">
+                    <label className="login-form-title" >Acesse sua conta</label>
+                    <div className="wrap-input">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="wrap-input">
+                        <label htmlFor="passoword">Senha</label>
+                        <input
+                            type="passoword"
+                            name="passoword"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label className="focus-input" data-placeholder="Email"></label>
+                    </div>
+                    <div className="container-login-form-btn">
+                        <button type="submit">Entrar</button>
+                    </div>
+                    <span className='txt1'>Ainda não tem cadastro?</span>
+                        <Link className="txt2" to="/cadastro">
+                            Cadastre-se
+                            </Link>
                 </div>
             </form>
         </div>
-    )
+    );
 }
-
-export default Login
